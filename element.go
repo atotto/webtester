3ea29d2b808fd6b920ea69d3ffbc2dcb0e4554b9
Ato Araki <ato.araki@gmail.com>
2023-06-12 15:03:54 +0900

    element: add wait for enabled

--- element.go
+++ element.go
@@ -56,3 +56,39 @@ func (e *Element) VerifyText(fn func(string, string) bool, expect string) *Eleme
 	}
 	return e
 }
+
+func (e *Element) WaitForEnabled() *Element {
+	e.Helper()
+
+	var enabled bool
+	var err error
+	ok := wait(func() bool {
+		enabled, err = e.elem.IsEnabled()
+		if err != nil {
+			e.Fatal(err)
+		}
+		return enabled
+	})
+	if !ok {
+		e.Fatal(err)
+	}
+	return e
+}
+
+func (e *Element) WaitForDisabled() *Element {
+	e.Helper()
+
+	var enabled bool
+	var err error
+	ok := wait(func() bool {
+		enabled, err = e.elem.IsEnabled()
+		if err != nil {
+			e.Fatal(err)
+		}
+		return !enabled
+	})
+	if !ok {
+		e.Fatal(err)
+	}
+	return e
+}
